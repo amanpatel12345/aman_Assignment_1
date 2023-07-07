efbd29fa0bedc6cb6a6555215dc234af97e86d5a
amanpatel12345 <138846262+amanpatel12345@users.noreply.github.com>
2023-07-07 10:18:12 +0530

    Add files via upload

---
+++ ASSIGNMENT_1.py
@@ -0,0 +1,7 @@
+n1 = float(input("Enter the marks for n1: "))
+n2 = float(input("Enter the marks for n2: "))
+n3 = float(input("Enter the marks for n3: "))
+
+average = (n1 + n2 + n3) / 3
+
+print("The average of {0}, {1}, and {2} is {3}".format(n1, n2, n3, average))
